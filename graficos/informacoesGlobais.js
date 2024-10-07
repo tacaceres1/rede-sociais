af84ca8a25b2f44f84361ce89bd5f54cdd4d09e0
tacaceres1 <160273307+tacaceres1@users.noreply.github.com>
2024-10-07 09:46:23 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,18 +11,10 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
-    
-
-   paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoas_mundo}bilhões </span> de pessoas e que aproximadamente <span>${pessoas_conectadas}bilhões </span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente`
-  
-
-    // código omitido
-
-const container = document.getElementById('graficos-container')
-container.appendChild(paragrafo)
-  }
-  
-
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
 
 vizualizarInformacoesGlobais()
